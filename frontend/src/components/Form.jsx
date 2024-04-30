eb1772def8b3be13f2f5f437cf604dd0abdbdc20
yawtalkstechs <yawtalkstechs@gmail.com>
2024-04-30 19:47:51 +0000

    Add functionality to home page: display notes, delete notes, and create notes
    * Add state variables for notes, content, and title in Home component
    * Implement useEffect hook to fetch notes on component load
    * Implement deleteNote function using

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -3,6 +3,7 @@ import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/Form.css";
+import LoadingIndicator from "./LoadingIndicator";
 
 function Form({ route, method }) {
   const [username, setUserName] = useState("");
@@ -50,6 +51,7 @@ function Form({ route, method }) {
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
       />
+      {loading &&  <LoadingIndicator />}
       <button className="form-button" type="submit">
         {name}
       </button>
