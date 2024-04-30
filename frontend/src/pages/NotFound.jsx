ef8b6923a0657d2286a4399fd7e6f492226e717b
yawtalkstechs <yawtalkstechs@gmail.com>
2024-04-30 00:22:25 +0000

    Frontend Added

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+function NotFound() {
+    return <div>
+        <h1>404 Not Found</h1>
+        <p>Hahahaha!!!</p>
+        <p>Sorry, The page you are looking for does not exist!!!</p>
+    </div>
+}
+
+export default NotFound
